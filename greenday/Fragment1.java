56565d76323f954be0a80e5c70d93288800c1b51
Grace <gj.gjonn@gmail.com>
2016-03-16 13:27:23 +0300

    CHanging names

---
+++ greenday/Fragment1.java
@@ -0,0 +1,29 @@
+package com.example.onikah.greenday;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+   public Fragment1() {
+
+   }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+    }
+}
